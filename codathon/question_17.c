a52650116a1edfdbb6406f8100235748c5277ffe
NPrabhat <prabhatnagdeve05@gmail.com>
2023-09-20 12:09:57 +0530

    codeathon

--- codathon/question_17.c
+++ codathon/question_17.c
@@ -6,16 +6,33 @@ multidimensional arrays
 #include<stdio.h>
 
 int main(){
-    int a[2][2],b[2][2],c[2][2];
+    int a[2][2]={{1,2},{1,2}},b[2][2]={{1,2},{1,2}},c[2][2];
     int i,j;
 
     for(i=0;i<2;i++){
         for(j=0;j<2;j++){
-            if(j=0 && j==0){
-                
+            if(i==0 && j==0){
+                c[i][j] = a[i][j]+b[i][j];
             }
+            else if (i==0 && j==1)
+            {
+                c[i][j] = a[i][j]+b[i][j];
+            }
+            else if (i==1 && j==0)
+            {
+                 c[i][j] = a[i][j]+b[i][j]; 
+            }
+            else if (i==1 && j==1)  
+            {
+                 c[i][j] = a[i][j]+b[i][j];
+            }                  
         }
     }
-
-
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+            printf("%d",c[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
