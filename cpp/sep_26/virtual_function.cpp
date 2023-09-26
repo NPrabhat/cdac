fc281e573211bb00b7fcb9609a9f23c929402b67
NPrabhat <prabhatnagdeve05@gmail.com>
2023-09-26 23:29:56 +0530

    Commiit of the day

---
+++ cpp/sep_26/virtual_function.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+//Function overriding
+
+class Base{
+    virtual void show(){
+        cout<<"Base class";
+    }
+};
+
+class Derived{
+    void show(){
+        cout<<"Derived class";
+    }
+};
+
+int main(){
+    Base *b;
+    Derived d;
+
+    p =&d;
+    p-> 
+
+
+}
